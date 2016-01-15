ccd2f56a7473364a9a34ed9138f130e8902f1cd4
Jiang Tao <imjiangtao@gmail.com>
2016-01-15 16:10:39 +0800

    update some

--- os/linux/source/source.sh
+++ os/linux/source/source.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export EDITOR=vim
+export LANG=zh_CN.UTF-8
 
 if [ -n "$WINDOWID" ]; then
-    TRANSPARENCY_HEX=$(printf 0x%x $((0xffffffff * 92 / 100)))
+    TRANSPARENCY_HEX=$(printf 0x%x $((0xffffffff * 95 / 100)))
     xprop -id "$WINDOWID" -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY "$TRANSPARENCY_HEX"
 fi
 
@@ -10,13 +11,15 @@ fi
 alias t='tmux'
 alias e='emacs -mm &'
 alias doc='cd /mnt/tao/Documents/'
-alias soc='cd /mnt/tao/Documents/C_SOC'
+alias soc='cd /mnt/tao/Documents/E_SOC'
 alias cus='cd /mnt/tao/Documents/Y_customers'
 alias ingenic='cd /mnt/tao/Documents/Y_customers/A_Ingenic'
 alias os='cd /mnt/tao/Documents/Y_customers/A_Ingenic/codes/os'
 alias l='cd /mnt/tao/Documents/Y_customers/A_Ingenic/codes/os/linux'
 alias a='cd /mnt/tao/Documents/Y_customers/A_Ingenic/codes/os/android'
 alias a41='cd /mnt/tao/Documents/Y_customers/A_Ingenic/codes/os/android/android-4.1'
+alias cgrep='find . -name .repo -prune -o -name .git -prune -o -type f \( -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "Kconfig" -o -name "Makefile" \) -print0 | xargs -0 grep --color -n'
+alias myclip='xclip -sel clip'
 
 alias | sed -E "s/^alias ([^=]+)='(.*)'$/alias \1 \2 \$*/g; s/'\\\''/'/g;" > ~/.emacs.d/eshell/alias
 
@@ -69,16 +72,13 @@ function gcc-mips-412-nopic() {
         export PATH=`echo $PATH | sed -e 's:/mnt/tao/Utils/compiler/mips\w*-gcc[^:]*/bin:/mnt/tao/Utils/compiler/mipsel-gcc412-glibc236-nopic-fp-20101020/bin:'`
 }
 
-# grep
-function cgrep() {
-	find -name .repo -prune -o -name .git -prune -o -type f \( -name '*.c' -o -name '*.h' \) -print0 | xargs -0 grep --color -n "$@"
-}
-function kgrep() {
-	find -name .repo -prune -o -name .git -prune -o -type f \( -name '*.c' -o -name '*.h' -o -name 'Kconfig' -o -name 'Makefile' \) -print0 | xargs -0 grep --color -n "$@"
-}
-
 # source (git clone https://github.com/rupa/z.git)
 source /mnt/tao/Utils/z.git/z.sh
 
 # add retmux
-[[ -z $TMUX ]] && export PATH=$PATH:/home/tao/Documents/B_Linux_App/codes/utils/retmux
+[[ -z $TMUX ]] && export PATH=$PATH:/mnt/tao/Documents/C_Linux_App/codes/utils/retmux
+
+# https://wiki.archlinux.de/title/GNOME#Tipps_und_Tricks
+export NO_AT_BRIDGE=1
+
+export TERM=xterm-256color
